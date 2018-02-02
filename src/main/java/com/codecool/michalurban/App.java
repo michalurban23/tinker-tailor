6563d85785fb04ef74c7ab26a8f80042d75c2af3
Michał Urban <michalurban23@gmail.com>
2018-02-02 21:23:38 +0100

    Add main App class

--- src/main/java/com/codecool/michalurban/App.java
+++ src/main/java/com/codecool/michalurban/App.java
@@ -1,13 +1,21 @@
 package com.codecool.michalurban;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.codecool.michalurban.game.Game;
+import com.codecool.michalurban.game.TinkerTailor;
+
+public class App {
+
+    public static void main( String[] args ) {
+
+        Game game = new TinkerTailor();
+        int players = 21; // N parameter
+        int count = 3; // K parameter
+
+        game.setup(players, count);
+        game.play();
+
+        System.out.println("Game Winner: " + game.getWinner());
+        System.out.println("Elimination order: " + game.getResults());
     }
+
 }
